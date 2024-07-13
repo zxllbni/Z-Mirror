3f1d90dba2db99c27548f51c3619317525e3b84a
SONETWORKOP <sonetworkop@gmail.com>
2024-07-13 09:41:43 +0530

    update.py

--- update.py
+++ update.py
@@ -64,7 +64,7 @@ if UPSTREAM_REPO:
     log_info('Fetching latest updates...')
     if update.returncode == 0:
         log_info('Successfully updated...')
-        log_info('Thanks For Using @Z_Mirror')
+        log_info('Thanks For Using @so_methods')
     else:
         log_error('Error while getting latest updates.')
         log_error('Check if entered UPSTREAM_REPO is valid or not!')
