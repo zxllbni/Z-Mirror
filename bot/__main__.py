93ccabf16a09b49c5db41d2e75480134cb42835c
SONETWORKOP <sonetworkop@gmail.com>
2024-07-13 09:40:01 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,7 +58,7 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Use me at: @so_methods'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
                        'Join: @Z_Mirror to use me.\n' \
